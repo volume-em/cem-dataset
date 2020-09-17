5c528ed3bad8864e2459b463550ffb5b0286d8f0
conradry <conradrw@nih.gov>
2020-09-17 13:26:28 -0400

    removed moco metrics and resnet

--- moco/metrics.py
+++
@@ -1,8 +0,0 @@
-import torch
-
-def alignment(x, y, alpha=2):
-    return (x - y).norm(p=2, dim=1).pow(alpha).mean().item()
-
-
-def uniformity(x, t=2):
-    return torch.pdist(x, p=2).pow(2).mul(-t).exp().mean().log().item()
