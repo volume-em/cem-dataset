92a7327c3e7411d1cd88c024a68e80fcfb7dc2eb
conradry <conradrw@nih.gov>
2022-04-15 12:58:22 -0400

    weights to zenodo

--- evaluation/resources/utils.py
+++ evaluation/resources/utils.py
@@ -1,9 +1,10 @@
+import torch
 import torch.hub
 
 __all__ = ['moco_to_unet_prefixes', 'load_pretrained_state_for_unet']
 
 cellemnet_moco_model_urls = {
-    'resnet50': 'https://www.dropbox.com/s/bqw4h2x23v3cgup/cellemnet_filtered_moco_v2_200ep.pth.tar?raw=1'
+    'resnet50': 'https://zenodo.org/record/6453140/files/cem500k_mocov2_resnet50_200ep.pth.tar?download=1'
 }
 
 imagenet_moco_model_urls = {
@@ -24,25 +25,62 @@ def moco_to_unet_prefixes(state_dict):
         
     return state_dict
 
+def swav_to_unet_prefixes(state_dict):
+    # rename moco pre-trained keys
+    for k in list(state_dict.keys()):
+        # retain only encoder_q up to before the embedding layer
+        if k.startswith('module.') and not k.startswith('module.projection_head'):
+            #for unet, we need to remove module.encoder_q. from the prefix
+            #and add encoder instead
+            state_dict['encoder.' + k[len("module."):]] = state_dict[k]
+
+        # delete renamed or unused k
+        del state_dict[k]
+        
+    return state_dict
+
+def pixpro_to_unet_prefixes(state_dict):
+    # rename pixpro pre-trained keys
+    for k in list(state_dict.keys()):
+        # retain only encoder_q up to before the embedding layer
+        if k.startswith('module.encoder.backbone.'):
+            #for unet, we need to remove module.encoder_q. from the prefix
+            #and add encoder instead
+            state_dict['encoder.' + k[len("module.encoder.backbone."):]] = state_dict[k]
+
+        # delete renamed or unused k
+        del state_dict[k]
+        
+    return state_dict
+
 
 def load_pretrained_state_for_unet(model_name='resnet50', pretraining='cellemnet_mocov2'):
     #validate the pretraining dataset name
-    if pretraining not in ['cellemnet_mocov2', 'imagenet_mocov2']:
+    if pretraining not in ['cellemnet_mocov2', 'imagenet_mocov2', 'cellemnet_pixpro']:
         raise Exception(f'Pretraining must be either cellemnet_mocov2 or imagenet_mocov2, got {pretraining}')
     
     #get the url
     if pretraining == 'cellemnet_mocov2':
         url = cellemnet_moco_model_urls[model_name]
+        model_state = torch.hub.load_state_dict_from_url(url)
+        state_dict = model_state['state_dict']
+        state_dict = moco_to_unet_prefixes(state_dict)
+        
+    elif pretraining == 'cellemnet_pixpro':
+        model_state = torch.load('/data/IASEM/conradrw/models/CEM500K_PixPro100/current.pth.tar')
+        state_dict = model_state['state_dict']
+        state_dict = pixpro_to_unet_prefixes(state_dict)
     else:
         url = imagenet_moco_model_urls[model_name]
+        state_dict = torch.hub.load_state_dict_from_url(url)
     
     #download and save the weights to TORCH_HOME; after initial download, weight are
     #loaded from that path instead
-    model_state = torch.hub.load_state_dict_from_url(url)
+    #model_state = torch.hub.load_state_dict_from_url(url)
     
     # rename moco pre-trained keys
-    state_dict = model_state['state_dict']
-    state_dict = moco_to_unet_prefixes(state_dict)
+    #state_dict = model_state['state_dict']
+    #state_dict = moco_to_unet_prefixes(state_dict)
         
     #return both the model state_dict and the norms used during training
     #if there are no norms, then we return None and will assume ImageNet
